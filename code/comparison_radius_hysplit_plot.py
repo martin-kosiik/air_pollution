fc9b1fd7810cf099149184460a8e5ff97d4b1f4e
Martin Kosík <martin.kosiik@gmail.com>
2023-03-19 20:45:51 +0100

    add new plots

--- code/comparison_radius_hysplit_plot.py
+++ code/comparison_radius_hysplit_plot.py
@@ -51,9 +51,28 @@ conc_adj = 4/30
 
 #(hysplit_pop_exposure*infant_share*phi_mg_per_ha*infat_mortality_increase*conc_adj*10).plot()
 
+
+
 hysplit_pop_exposure = hysplit_pop_exposure*infant_share*phi_mg_per_ha*infat_mortality_increase*conc_adj
 
-hysplit_pop_exposure.plot()
+
+ax = plt.axes(projection = ccrs.PlateCarree())
+ax.set_extent([73.5, 78, 27.0, 32], crs=ccrs.PlateCarree())
+ax.coastlines(resolution='50m')
+ax.add_feature(cf.BORDERS, linewidth=2.5)
+ax.add_feature(cf.STATES, linewidth=0.5)
+gl = ax.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False,
+              linewidth=0.3, color='gray', alpha=0.5, linestyle='--')
+gl.top_labels = False
+gl.right_labels = False
+ax.plot(77.216721, 28.644800, 'bo', markersize=2, transform=ccrs.PlateCarree())
+ax.text(77, 28.3, 'New Delhi', transform=ccrs.PlateCarree())
+hysplit_pop_exposure.plot(alpha=1, transform=ccrs.PlateCarree())
+plt.title('Expected infant deaths per hectare of burned land')
+plt.savefig("figures/exp_inf_deaths_no_interpolation.png")
+
+
+
 (radius_pop_exposure*infant_share*infant_mort_increase_per_ha_burned).plot()
 radius_pop_exposure.plot()
 population_raster_cropped = population_raster.rio.clip_box(
@@ -65,9 +84,6 @@ population_raster_cropped = population_raster.rio.clip_box(
 )
 
 
-ccrs.epsg(4326)
-ccrs.epsg(26910)
-
 hysplit_pop_exposure_matched = hysplit_pop_exposure.rio.reproject_match(population_raster_cropped, 
                                                            resampling=rasterio.enums.Resampling.cubic)
 
@@ -77,13 +93,28 @@ radius_pop_exposure_matched = radius_pop_exposure.rio.reproject_match(population
 
 
 
+ax = plt.axes(projection = ccrs.PlateCarree())
+ax.set_extent([73.5, 78, 27.0, 32], crs=ccrs.PlateCarree())
+ax.coastlines(resolution='50m')
+ax.add_feature(cf.BORDERS, linewidth=2.5)
+ax.add_feature(cf.STATES, linewidth=0.5)
+gl = ax.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False,
+              linewidth=0.3, color='gray', alpha=0.5, linestyle='--')
+gl.top_labels = False
+gl.right_labels = False
+ax.plot(77.216721, 28.644800, 'bo', markersize=2, transform=ccrs.PlateCarree())
+ax.text(77, 28.3, 'New Delhi', transform=ccrs.PlateCarree())
+hysplit_pop_exposure_matched.plot(alpha=1, transform=ccrs.PlateCarree())
+plt.title('Expected infant deaths per hectare of burned land')
+plt.savefig("figures/exp_inf_deaths_interpolated.png")
+
+
+
+
 
 burned_area = rioxarray.open_rasterio("data/burned_area/MCD64A1.006_Burn_Date_doy2019274_aid0001 (1).tif") 
 #burned_area = burned_area.rio.set_crs(rasterio.crs.CRS.from_epsg(4326), inplace = False) #
 burned_area = (burned_area > 0)*1
-burned_area.rio.crs
-burned_area.rio.nodata
-burned_area.rio.nodata
 
 burned_area_reproject = burned_area.rio.reproject(rasterio.crs.CRS.from_epsg(4326))
 
@@ -91,10 +122,45 @@ burned_area_reproject = burned_area.rio.reproject(rasterio.crs.CRS.from_epsg(432
 
 burned_area_matched = burned_area_reproject.rio.reproject_match(population_raster_cropped, 
                                                            resampling=rasterio.enums.Resampling.cubic)
-burned_area_matched.plot(ylim=[28,32])
 
 
 burned_area_matched = (burned_area_matched > 0)*1
+
+
+
+hysplit_pop_exposure_matched_masked = hysplit_pop_exposure_matched.where(burned_area_matched >0)
+radius_pop_exposure_matched_masked = radius_pop_exposure_matched.where(burned_area_matched >0)
+
+
+ax = plt.axes(projection = ccrs.PlateCarree())
+ax.set_extent([73.5, 78, 27.0, 32], crs=ccrs.PlateCarree())
+ax.coastlines(resolution='50m')
+ax.add_feature(cf.BORDERS, linewidth=2.5)
+ax.add_feature(cf.STATES, linewidth=0.5)
+gl = ax.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False,
+              linewidth=0.3, color='gray', alpha=0.5, linestyle='--')
+gl.top_labels = False
+gl.right_labels = False
+ax.plot(77.216721, 28.644800, 'bo', markersize=2, transform=ccrs.PlateCarree())
+ax.text(77, 28.3, 'New Delhi', transform=ccrs.PlateCarree())
+hysplit_pop_exposure_matched_masked.plot(alpha=1, transform=ccrs.PlateCarree())
+plt.title('Expected infant deaths per hectare of burned land')
+plt.savefig("figures/exp_inf_deaths_interpolated_filtered.png")
+
+
+
+bins =np.linspace(start = 0, stop = 0.045, num = 50)
+hysplit_pop_exposure_matched.plot.hist(bins = bins, xlim=[0, 0.045], alpha=0.55, density=True)
+hysplit_pop_exposure_matched_masked.plot.hist(bins = bins,xlim=[0, 0.045], alpha=0.55, density=True)
+plt.legend(['All pixels', 'Only pixels with burned land'], loc="upper left")
+plt.xlabel('Expected infant deaths per hectare of burned land')
+plt.ylabel('Probablity density')
+plt.title('')
+plt.savefig("figures/hist_comparison_burned_area_exp_inf_deaths.png")
+
+
+
+
 
 
 burned_area.plot()
@@ -112,14 +178,6 @@ plt.savefig("figures/radius_approach/hist_comparison_exp_inf_deaths.png")
 
 
 
-ax = plt.axes(projection = ccrs.Mercator())
-ax.add_feature(cf.COASTLINE)
-ax.add_feature(cf.BORDERS)
-p = radius_pop_exposure_matched.plot(alpha=0.5, transform=ccrs.Mercator())
-
-plt.title('Expected infant deaths per hectare of burned land')
-plt.ylabel('latitude')
-plt.xlabel('longitude')
 
 
 
@@ -160,8 +218,6 @@ plt.savefig("figures/radius_approach/hysplit_pop_exposure_exp_inf_deaths.png")
 
 
 
-hysplit_pop_exposure_matched_masked = hysplit_pop_exposure_matched.where(burned_area_matched >0)
-radius_pop_exposure_matched_masked = radius_pop_exposure_matched.where(burned_area_matched >0)
 
 hysplit_pop_exposure_matched_masked.plot()
 radius_pop_exposure_matched_masked.plot()
